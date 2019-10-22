f277392f5f5c57469f1181b145be6a30011bcb8b
embarq <embarq.dev@gmail.com>
2019-10-22 16:59:00 +0300

    update env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,14 @@ const remoteConf = require('../../.runtimeconfig.json');
 
 export const environment = {
   production: true,
-  firebase: remoteConf.webclient_conf,
+  firebase: {
+    apiKey: remoteConf.webclient_conf.api_key,
+    appId: remoteConf.webclient_conf.app_id,
+    authDomain: remoteConf.webclient_conf.auth_domain,
+    databaseUrl: remoteConf.webclient_conf.database_url,
+    messagingSenderId: remoteConf.webclient_conf.messaging_sender_id,
+    projectId: remoteConf.webclient_conf.project_id,
+    storageBucket: remoteConf.webclient_conf.storage_bucket
+  },
   remoteAssetsCdn: remoteConf.rapid_api.cdn_url
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 const remoteConf = require('../../.runtimeconfig.json');
 
 export const environment = {
   production: false,
-  firebase: remoteConf.webclient_conf,
+  firebase: {
+    apiKey: remoteConf.webclient_conf.api_key,
+    appId: remoteConf.webclient_conf.app_id,
+    authDomain: remoteConf.webclient_conf.auth_domain,
+    databaseUrl: remoteConf.webclient_conf.database_url,
+    messagingSenderId: remoteConf.webclient_conf.messaging_sender_id,
+    projectId: remoteConf.webclient_conf.project_id,
+    storageBucket: remoteConf.webclient_conf.storage_bucket
+  },
   remoteAssetsCdn: remoteConf.rapid_api.cdn_url
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
