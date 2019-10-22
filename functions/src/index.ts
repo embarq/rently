6a69ef902632d9896d367b21b143dc532bbff9b3
embarq <embarq.dev@gmail.com>
2019-10-22 14:23:31 +0300

    implement ssr function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,6 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+const universal = require(`${ process.cwd() }/dist/server`);
+
+export const ssr = functions
+  .runWith({ memory: '512MB' })
+  .https.onRequest(universal.app);
