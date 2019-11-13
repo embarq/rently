359f8da1e1193aa891b43757e2f4d3104cd144e4
embarq <embarq.dev@gmail.com>
2019-11-13 08:36:59 +0200

    app: remove router config display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, ChangeDetectionStrategy, ApplicationRef } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'rty-root',
-  template: `
-    <rty-layout>
-      <router-outlet></router-outlet>
-    </rty-layout>
-    <pre style="display:none">{{ routerConfig }}</pre>
-  `,
-  styles: []
+  template: `<rty-layout><router-outlet></router-outlet></rty-layout>`,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  routerConfig: string;
-  constructor(router: Router) {
-    this.routerConfig = JSON.stringify(router.config);
-    router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        try {
-          this.routerConfig = JSON.stringify(router.config);
-        } catch (error) {
+  constructor(
+    router: Router,
+    zone: NgZone,
+    appRef: ApplicationRef
+  ) {
+    zone.runOutsideAngular(() => {
+      appRef.isStable.subscribe((isStable) => console.log('App is stable', isStable));
+    });
+    zone.runOutsideAngular(() => {
+      router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          if (event.urlAfterRedirects === '/not-found') {
+            console.log('[route not found]:', JSON.stringify(event));
+          } else {
+            console.log('[route recognized]:', JSON.stringify(event));
+          }
         }
-
-        if (event.urlAfterRedirects === '/not-found') {
-          console.log('[route not found]:', JSON.stringify(event));
-        } else {
-          console.log('[route recognized]:', JSON.stringify(event));
-        }
-      }
+      });
     })
   }
 }
